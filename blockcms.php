a1530f05261ddc65d976d348c704120f70e7365b
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-08-23 10:59:08 +0200

    //Version update

--- blockcms.php
+++ blockcms.php
@@ -39,7 +39,7 @@ class BlockCms extends Module
 	{
 		$this->name = 'blockcms';
 		$this->tab = 'front_office_features';
-		$this->version = '1.9';
+		$this->version = '2.0.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
